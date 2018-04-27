46a1a77ebc7cadaea4cd69fa81735c13962c761b
yokselzok <yokselzok@gmail.com>
2018-04-27 15:04:47 +0300

    Add walls

--- src/data/config.js
+++ src/data/config.js
@@ -1,23 +1,69 @@
 const config = {
-    top: {
-        width: [
-            45,
-            150,
-            90,
-            45
-        ],
-        height: 60,
-        deep: 40
-    },
-    bottom: {
-        width: [
-            60,
-            150,
-            90,
-            45
-        ],
-        height: 100,
-        deep: 60
+    walls: {
+        front: {
+            top: {
+                width: [
+                    60,
+                    150,
+                    110,
+                    65
+                ],
+                height: 60,
+                deep: 40
+            },
+            bottom: {
+                width: [
+                    60,
+                    150,
+                    90,
+                    45
+                ],
+                height: 100,
+                deep: 60
+            },
+        },
+        left: {
+            top: {
+                width: [
+                    60,
+                    50,
+                    90,
+                    65
+                ],
+                height: 60,
+                deep: 40
+            },
+            bottom: {
+                width: [
+                    60,
+                    50,
+                    90,
+                    45
+                ],
+                height: 100,
+                deep: 60
+            },
+        },
+        right: {
+            top: {
+                width: [
+                    60,
+                    150,
+                    65
+                ],
+                height: 60,
+                deep: 40
+            },
+            bottom: {
+                width: [
+                    60,
+                    150,
+                    45
+                ],
+                height: 100,
+                deep: 60
+            },
+        }
     },
     verticalGap: 50
 }
